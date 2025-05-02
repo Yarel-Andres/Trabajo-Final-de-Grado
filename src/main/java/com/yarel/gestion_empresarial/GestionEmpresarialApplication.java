0c7dd54285d3d092dd84917f256885e99011b897
Yarel <andres.sanchez221@gmail.com>
2025-05-02 13:00:35 +0200

    Pimer Commit de mi Proyecto. He tenido que volver a crear el repositorio de GitHub y un proyecto nuevo debido a problemas con el codigo.

---
+++ src/main/java/com/yarel/gestion_empresarial/GestionEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package com.yarel.gestion_empresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmpresarialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEmpresarialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yarel/gestion_empresarial/GestionEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarel.gestion_empresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmpresarialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
