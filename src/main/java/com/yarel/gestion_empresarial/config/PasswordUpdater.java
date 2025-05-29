0b4a4674cf67512b137a3349e56fe94ba1a25969
Yarel-Andres <andres.sanchez221@gmail.com>
2025-05-29 13:10:15 +0200

    31º Commit. Modificacion del archivo properties y añadido json para comenzar el despligue en railway

--- src/main/java/com/yarel/gestion_empresarial/config/PasswordUpdater.java
+++ src/main/java/com/yarel/gestion_empresarial/config/PasswordUpdater.java
@@ -18,8 +18,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-// @Component - COMENTADO TEMPORALMENTE PARA TESTING
-@ConditionalOnProperty(name = "app.password-updater.enabled", havingValue = "true")
+@Component // - COMENTADO TEMPORALMENTE PARA TESTING
+//@ConditionalOnProperty(name = "app.password-updater.enabled", havingValue = "true")
 public class PasswordUpdater implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(PasswordUpdater.class);
